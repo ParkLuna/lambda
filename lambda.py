abd047558e2f2200b17c29c9b02ca6745716a426
ParkLuna <141165285+ParkLuna@users.noreply.github.com>
2023-09-09 01:51:52 +0530

    lambda.py

--- lambda.py
+++ lambda.py
@@ -1,4 +1,12 @@
-ad=lambda x: x + 25
-sample_input = 10
-result = ad(sample_input)
-print(result) 
+try:
+    value_to_add = float(input("Enter a number to add to 25: "))
+except ValueError:
+    print("Invalid input. Please enter a valid number.")
+    exit()
+
+add_25 = lambda x: x + 25
+
+result = add_25(value_to_add)
+
+print(f"Result: {result}")
+
